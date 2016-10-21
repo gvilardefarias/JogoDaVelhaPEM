50fceb34ccec24e52167df322ea93498ba98c9e9
Israel Neto <israelbcneto@hotmail.com>
2016-10-21 10:48:10 -0200

    Correção de bugs para coleta do endereço local de IP no Windows, Linux e MAC

--- Cliente-Servidor.py
+++ Cliente-Servidor.py
@@ -3,8 +3,6 @@ from flask import Flask, request
 from flask_cors import CORS, cross_origin
 from random import randint
 import socket
-import fcntl
-import struct
 import requests
 import Nucleo
 import IA
@@ -51,12 +49,11 @@ def setJogada():
     requests.get("http://" + ipAdversario + "/setJogada/" + str(minhaJogada))
 
 def get_ip_address():
-    s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-    return socket.inet_ntoa(fcntl.ioctl(
-        s.fileno(),
-        0x8915,  # SIOCGIFADDR
-        struct.pack('256s', 'eth0'[:15])
-    )[20:24])
+    return [l for l in ([ip for ip in socket.gethostbyname_ex(socket.gethostname())[2] if not ip.startswith("127.")][:1], [[(s.connect(('8.8.8.8', 53)), s.getsockname()[0], s.close()) for s in [socket.socket(socket.AF_INET, socket.SOCK_DGRAM)]][0][1]]) if l][0][0]
+
+@app.route("/test")
+def test():
+    return "ok"
 
 @app.route("/pegarPeca")
 def pegarPeca():
